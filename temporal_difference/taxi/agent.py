2c51ffdfc851a60ff464c96d1eca863a4fb18878
Galen Ballew <galen.ballew@gmail.com>
2018-09-07 12:49:09 -0500

    adding intial files

---
+++ temporal_difference/taxi/agent.py
@@ -0,0 +1,61 @@
+import numpy as np
+from collections import defaultdict
+import math
+
+class Agent:
+
+    def __init__(self, nA=6, observation_space=500):
+        """ Initialize agent.
+
+        Params
+        ======
+        - nA: number of actions available to the agent
+        """
+        self.nA = nA
+        self.Q = np.zeros((observation_space, self.nA))
+        self.alpha = 0.7
+        self.alpha_decay = 0.9999
+        self.epsilon = 1.0
+        self.gamma = 0.90
+        self.i_episode = 0
+
+    def epsilon_greedy_probs(self, Q_s, i_episode):
+        """ obtains the action probabilities corresponding to epsilon-greedy policy """
+        policy_s = np.ones(self.nA) * self.epsilon / self.nA
+        policy_s[np.argmax(Q_s)] = 1 - self.epsilon + (self.epsilon / self.nA)
+        return policy_s
+
+    def update_Q(self, Qsa, Qsa_next, reward, alpha, gamma):
+        """ updates the action-value function estimate using the most recent time step """
+        return Qsa + (alpha * (reward + (gamma * Qsa_next) - Qsa))
+
+    def select_action(self, state):
+        """ Given the state, select an action.
+
+        Params
+        ======
+        - state: the current state of the environment
+
+        Returns
+        =======
+        - action: an integer, compatible with the task's action space
+        """
+        # get epsilon-greedy action probabilities
+        self.policy_s = self.epsilon_greedy_probs(self.Q[state], self.i_episode)
+        # pick next action A
+        action = np.random.choice(np.arange(self.nA), p=self.policy_s)
+        return action
+
+    def step(self, state, action, reward, next_state, done):
+        """ Update the agent's knowledge, using the most recently sampled tuple.
+
+        Params
+        ======
+        - state: the previous state of the environment
+        - action: the agent's previous choice of action
+        - reward: last reward received
+        - next_state: the current state of the environment
+        - done: whether the episode is complete (True or False)
+        """
+        # Q learning
+        self.Q[state][action] = self.update_Q(self.Q[state][action], self.Q[next_state][action], reward, self.alpha, self.gamma)
